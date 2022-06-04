67aff46149cd552d344eb5672ecd58426c60f497
developerdesigner18 <developer.designer18@gmail.com>
2022-06-04 13:12:49 +0530

    Created Solution page

--- components/Common/ServiceCard/ServiceCard.js
+++ components/Common/ServiceCard/ServiceCard.js
@@ -1,5 +1,36 @@
 const ServiceCard = () => {
-    return <div>ServiceCard</div>;
+    return (
+        <>
+            <div className="container servicecard">
+                <div className="row">
+                    <div className="col-lg-6 col-md-12">
+                        <div className="solution-image">
+                            <img
+                                src="/images/solutions/eccomerce.png"
+                                alt="image"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="col-lg-6 col-md-12">
+                        <div className="about-content service">
+                            <div className="content">
+                                <h2>Ecommerce, Retail And B2B</h2>
+                                <p>
+                                    Lorem Ipsum is simply dummy text of the
+                                    printing and typesetting industry. Lorem
+                                    Ipsum has been the industry's standard dummy
+                                    text ever since the 1500s, when an unknown
+                                    printer took a galley of type and scrambled
+                                    it to make a type specimen book
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default ServiceCard;
