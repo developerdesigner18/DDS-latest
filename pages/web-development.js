caba2966ef937da6b0999a2e82f545e3842aa71b
developerdesigner18 <developer.designer18@gmail.com>
2022-06-07 18:17:26 +0530

    small fixes

--- pages/web-development.js
+++ pages/web-development.js
@@ -1,6 +1,6 @@
 import Navbar from "../components/_App/Navbar/Navbar";
 import PageBanner from "../components/Common/PageBanner/PageBanner";
-import ServiceCard from "../components/Common/ServiceCard/ServiceCard/ServiceCard";
+import ServiceCard from "../components/Common/ServiceCard/ServiceCard";
 import InfoCard from "../components/Common/InfoCard/InfoCard";
 import WebDevelopmentServices from "../components/WebDevelopment/WebDevelopmentServices/WebDevelopmentServices";
 import TechnologyStack from "../components/WebDevelopment/TechnologyStack/TechnologyStack";
