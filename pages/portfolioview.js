83b1fbbe79683cee0f9539e8a1c0f1396e0c36ae
developerdesigner18 <developer.designer18@gmail.com>
2022-06-10 17:59:58 +0530

    New changes on Portfolio page

--- pages/portfolioview.js
+++ pages/portfolioview.js
@@ -1,21 +1,15 @@
-import Navbar from "../components/_App/Navbar/Navbar";
 import PageBanner from "../components/Common/PageBanner/PageBanner";
-import Footer from "../components/_App/Footer/Footer";
 import PortfolioDetails from "../components/ProtfolioView/PortfolioDetails";
 import Moreprofile from "../components/ProtfolioView/Moreprofile";
 
 const portfolioview = () => {
     return (
         <>
-            <Navbar />
-
             <PageBanner pageTitle="Portfolio" />
 
             <PortfolioDetails />
 
             <Moreprofile />
-
-            <Footer />
         </>
     );
 };
