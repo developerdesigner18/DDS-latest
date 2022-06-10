4a3b52066b5e6014e7d8c619ba05c147d9bc6b95
developerdesigner18 <developer.designer18@gmail.com>
2022-06-10 12:26:16 +0530

    Update solutions page info card, service, and quote

--- components/Solutions/GetAQuote/GetAQuote.js
+++ components/Solutions/GetAQuote/GetAQuote.js
@@ -1,9 +1,44 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
 const GetAQuote = () => {
+    // Form initial state
+    const INITIAL_STATE = {
+        name: "",
+        email: "",
+        number: "",
+        subject: "",
+        text: "",
+    };
+    const [contact, setContact] = useState(INITIAL_STATE);
+    const { register, handleSubmit, errors } = useForm();
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setContact((prevState) => ({ ...prevState, [name]: value }));
+        console.log(contact);
+    };
+
+    const onSubmit = async (e) => {
+        // e.preventDefault();
+        try {
+            const url = `${baseUrl}/api/contact`;
+            const { name, email, number, subject, text } = contact;
+            const payload = { name, email, number, subject, text };
+            await axios.post(url, payload);
+            console.log(url);
+            setContact(INITIAL_STATE);
+            alertContent();
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <>
             <div className="container servicecard">
                 <div className="row quote">
-                    <div className="col-lg-6 col-md-12">
+                    <div className="col-lg-5 col-md-12">
                         <div className="solution-image">
                             <img
                                 src="/images/solutions/quote.png"
@@ -15,12 +50,132 @@ const GetAQuote = () => {
                     <div className="col-lg-6 col-md-12">
                         <div className="about-content service">
                             <div className="content">
-                                <span>Get A Free Quote</span>
+                                <span className="subheading">
+                                    Get A Free Quote
+                                </span>
                                 <h2>Get quote from us</h2>
                                 <p>
                                     Lorem Ipsum is simply dummy text of the
                                     printing and typesetting industry.
                                 </p>
+                                <div>
+                                    <form
+                                        id="contactForm"
+                                        onSubmit={handleSubmit(onSubmit)}
+                                    >
+                                        <div className="row">
+                                            <div className="col-lg-6 col-md-6">
+                                                <div className="form-group quote">
+                                                    <label>Full Name</label>
+                                                    <input
+                                                        type="text"
+                                                        name="name"
+                                                        className="form-control"
+                                                        value={contact.name}
+                                                        onChange={handleChange}
+                                                        ref={register({
+                                                            required: true,
+                                                        })}
+                                                    />
+                                                    <div
+                                                        className="invalid-feedback"
+                                                        style={{
+                                                            display: "block",
+                                                        }}
+                                                    >
+                                                        {errors.name &&
+                                                            "Name is required."}
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div className="col-lg-6 col-md-6">
+                                                <div className="form-group quote">
+                                                    <label>Website</label>
+                                                    <input
+                                                        type="text"
+                                                        name="email"
+                                                        className="form-control"
+                                                        value={contact.email}
+                                                        onChange={handleChange}
+                                                        ref={register({
+                                                            required: true,
+                                                            pattern:
+                                                                /^\S+@\S+$/i,
+                                                        })}
+                                                    />
+                                                    <div
+                                                        className="invalid-feedback"
+                                                        style={{
+                                                            display: "block",
+                                                        }}
+                                                    >
+                                                        {errors.email &&
+                                                            "Email is required."}
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div className="col-lg-6 col-md-6">
+                                                <div className="form-group quote">
+                                                    <label>Phone</label>
+                                                    <input
+                                                        type="text"
+                                                        name="number"
+                                                        className="form-control"
+                                                        value={contact.number}
+                                                        onChange={handleChange}
+                                                        ref={register({
+                                                            required: true,
+                                                        })}
+                                                    />
+                                                    <div
+                                                        className="invalid-feedback"
+                                                        style={{
+                                                            display: "block",
+                                                        }}
+                                                    >
+                                                        {errors.number &&
+                                                            "Number is required."}
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div className="col-lg-6 col-md-6">
+                                                <div className="form-group quote">
+                                                    <label>Email</label>
+                                                    <input
+                                                        type="text"
+                                                        name="subject"
+                                                        className="form-control"
+                                                        value={contact.subject}
+                                                        onChange={handleChange}
+                                                        ref={register({
+                                                            required: true,
+                                                        })}
+                                                    />
+                                                    <div
+                                                        className="invalid-feedback"
+                                                        style={{
+                                                            display: "block",
+                                                        }}
+                                                    >
+                                                        {errors.subject &&
+                                                            "Subject is required."}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="col-lg-12 col-sm-12">
+                                                <button
+                                                    type="submit"
+                                                    className="default-btn"
+                                                >
+                                                    Get A Quote
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
