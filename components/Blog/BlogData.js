72a2a0395ed468aba8d654b45b56a40c859cd283
developerdesigner18 <developer.designer18@gmail.com>
2022-06-11 12:25:55 +0530

    Worked on Blog page

---
+++ components/Blog/BlogData.js
@@ -0,0 +1,24 @@
+const BlogData = [
+    {
+        blogimg: "/images/blog/blog.png",
+        title: "Online blogs You should Read",
+        date: "Feb 20, 2022",
+    },
+    {
+        blogimg: "/images/blog/seo.png",
+        title: "Online blogs You should Read",
+        date: "Feb 20, 2022",
+    },
+    {
+        blogimg: "/images/blog/creative.png",
+        title: "Online blogs You should Read",
+        date: "Feb 20, 2022",
+    },
+    {
+        blogimg: "/images/blog/social.png",
+        title: "Online blogs You should Read",
+        date: "Feb 20, 2022",
+    },
+];
+
+export default BlogData;
