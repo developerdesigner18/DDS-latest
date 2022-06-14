bd59d95971cf9696c2fea923ccaecbb41c3b4080
developerdesigner18 <developer.designer18@gmail.com>
2022-06-14 12:45:06 +0530

    updated homepage, small refactor

--- components/Home/IndsustriesWeServe/IndustriesWeServe.js
+++ components/Home/IndsustriesWeServe/IndustriesWeServe.js
@@ -4,20 +4,68 @@ const OwlCarousel = dynamic(import("react-owl-carousel3"));
 
 const industries = [
     {
-        image: "/images/industries/e-commerce.png",
+        image: "/images/industries/e-commerce.webp",
         title: "E-Commerce",
     },
     {
-        image: "/images/industries/b2b.png",
+        image: "/images/industries/b2b.webp",
         title: "Business to Business",
     },
     {
-        image: "/images/industries/non-profit.png",
+        image: "/images/industries/non-profit.webp",
         title: "Non-Profit",
     },
     {
-        image: "/images/industries/startup.png",
-        title: "Startup",
+        image: "/images/industries/real-estate.webp",
+        title: "Real Estate & Property",
+    },
+    {
+        image: "/images/industries/social-networking.webp",
+        title: "Social Networking",
+    },
+    {
+        image: "/images/industries/food-and-restaurant.jpg",
+        title: "Food & Restaurant",
+    },
+    {
+        image: "/images/industries/banking-and-finance.webp",
+        title: "Banking & Finance",
+    },
+    {
+        image: "/images/industries/education.webp",
+        title: "Education",
+    },
+    {
+        image: "/images/industries/travel.webp",
+        title: "Travel",
+    },
+    {
+        image: "/images/industries/media-and-entertainment.webp",
+        title: "Media & Entertainment",
+    },
+    {
+        image: "/images/industries/healthcare-and-fitness.webp",
+        title: "Healthcare & Fitness",
+    },
+    {
+        image: "/images/industries/transport-and-automotive.webp",
+        title: "Transport & Automotive",
+    },
+    {
+        image: "/images/industries/gaming-and-leisure.webp",
+        title: "Gaming & Leisure",
+    },
+    {
+        image: "/images/industries/directory-and-organization.webp",
+        title: "Directory & Organisation",
+    },
+    {
+        image: "/images/industries/event-and-tickets.webp",
+        title: "Event & Tickets",
+    },
+    {
+        image: "/images/industries/sports.webp",
+        title: "Sports",
     },
 ];
 
--- components/Portfolio/UserPortfolio.js
+++ components/Portfolio/UserPortfolio.js
@@ -1,6 +1,37 @@
-import React, { useState } from "react";
 import Link from "next/link";
-import UserData from "./UserData";
+
+const users = [
+    {
+        userImg: "/images/portfolio/group1.png",
+        content:
+            "Contrary to popular belief, Lorem Ipsum is not simply random text.",
+    },
+    {
+        userImg: "/images/portfolio/group2.png",
+        content:
+            "Contrary to popular belief, Lorem Ipsum is not simply random text.",
+    },
+    {
+        userImg: "/images/portfolio/group3.png",
+        content:
+            "Contrary to popular belief, Lorem Ipsum is not simply random text.",
+    },
+    {
+        userImg: "/images/portfolio/group4.png",
+        content:
+            "Contrary to popular belief, Lorem Ipsum is not simply random text.",
+    },
+    {
+        userImg: "/images/portfolio/group5.png",
+        content:
+            "Contrary to popular belief, Lorem Ipsum is not simply random text.",
+    },
+    {
+        userImg: "/images/portfolio/group6.png",
+        content:
+            "Contrary to popular belief, Lorem Ipsum is not simply random text.",
+    },
+];
 
 const UserPortfolio = () => {
     return (
@@ -8,7 +39,7 @@ const UserPortfolio = () => {
             <div className="portfolio-area ptb-100">
                 <div className="container">
                     <div className="row">
-                        {UserData.map((elm) => {
+                        {users.map((elm) => {
                             return (
                                 <>
                                     <div className="col-lg-4 col-md-6 col-sm-6">
