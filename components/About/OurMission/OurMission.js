a6221c586ab569c591f0919eb88ddef298d928b1
developerdesigner18 <developer.designer18@gmail.com>
2022-06-18 11:23:20 +0530

    created OurVision, fixe Partners component

--- components/About/OurMission/OurMission.js
+++ components/About/OurMission/OurMission.js
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 const OurMission = () => {
     return (
-        <div className="ptb-100 bg-f9f9f9">
+        <div className="our-mission-area ptb-100 bg-f9f9f9">
             <div className="container">
+                <h3>Our Mission</h3>
                 <div className="our-mission">
                     <div className="left-img">
                         <img src="/images/our-mission/left.png" alt="" />
                     </div>
                     <div className="error-content">
-                        <h3>Our Mission</h3>
                         <p>
                             We are a client-centric company who believes in
                             going above and beyond to meet your demands and
--- components/About/OurVision/OurVision.js
+++ components/About/OurVision/OurVision.js
@@ -1,5 +1,56 @@
 const OurVision = () => {
-    return <div>OurVision</div>;
+    return (
+        <div className="our-vision-area ptb-100">
+            <div className="container">
+                <div className="row align-items-center">
+                    <h3>Our Vision</h3>
+                    <div className="col-lg-6 col-md-12">
+                        <div className="our-vision-content">
+                            <div className="text">
+                                <p>
+                                    In this digital age, technology is rapidly
+                                    evolving, and we want to make sure that we
+                                    stay updated with current trends so that we
+                                    can deliver exceptional service. We value
+                                    technical innovation, lifelong learning, and
+                                    adaptability in order to give the finest
+                                    digital solutions to our clients.
+                                </p>
+
+                                <ul className="features-list">
+                                    <li>
+                                        <i className="flaticon-speed"></i> Be
+                                        the destination for entrepreneurs to
+                                        make their idea a reality, no matter how
+                                        they want to travel through the digital
+                                        world.
+                                    </li>
+                                    <li>
+                                        <i className="flaticon-speed"></i> To
+                                        provide access to all latest
+                                        technologies through our offices and
+                                        digital presence.
+                                    </li>
+                                    <li>
+                                        <i className="flaticon-speed"></i>{" "}
+                                        Inspire the world with our cutting-edge
+                                        technology, products, and design that
+                                        improve people's lives and contribute to
+                                        socio - economic growth.
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-6 col-md-12">
+                        <div className="our-vision-img">
+                            <img src="/images/our-vision.png" alt="image" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default OurVision;
